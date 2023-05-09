a1d909076c480d92535e10622ba264870bcd3dd8
arsuhinars <arsuhinars@gmail.com>
2023-05-09 22:20:23 +0400

    ✨ Added views for friends methods

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,27 @@
 from django.urls import path
 
-from api.views import get_user_by_id, register_user, search_users
+from api.views import (
+    FriendView,
+    accept_invite,
+    decline_invite,
+    get_friends,
+    get_incoming_invites,
+    get_outcoming_invites,
+    get_user_by_id,
+    invite_friend,
+    register_user,
+    search_users,
+)
 
 urlpatterns = [
     path('registration', register_user),
     path('user/<int:user_id>', get_user_by_id),
-    path('users/search', search_users)
+    path('users/search', search_users),
+    path('friend/<int:user_id>', FriendView.as_view()),
+    path('friend/<int:user_id>/invite', invite_friend),
+    path('friend/<int:user_id>/accept', accept_invite),
+    path('friend/<int:user_id>/decline', decline_invite),
+    path('friends', get_friends),
+    path('friends/incoming', get_incoming_invites),
+    path('friends/outcoming', get_outcoming_invites),
 ]
--- api/views.py
+++ api/views.py
@@ -3,12 +3,13 @@ from django.http import JsonResponse
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.exceptions import NotFound, PermissionDenied
 from rest_framework.parsers import JSONParser
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.request import Request
+from rest_framework.views import APIView
 
 from api.exceptions import InvalidFormatException
 from api.serializers import UserCreateSerializer
-from api.services import user_service
+from api.services import friends_service, user_service
 
 
 @api_view(['POST'])
@@ -23,7 +24,6 @@ def register_user(request: Request):
 
 
 @api_view(['GET'])
-@permission_classes([AllowAny])
 def get_user_by_id(request: Request, user_id: int):
     user_dao = user_service.get_by_id(user_id)
     if user_dao is None:
@@ -32,7 +32,6 @@ def get_user_by_id(request: Request, user_id: int):
 
 
 @api_view(['GET'])
-@permission_classes([AllowAny])
 def search_users(request: Request):
     name_query = request.query_params.get('name')
     if name_query is None or len(name_query) == 0:
@@ -50,3 +49,73 @@ def search_users(request: Request):
         )),
         safe=False
     )
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def invite_friend(request: Request, user_id: int):
+    friends_service.invite_friend(request.user.id, user_id)
+    return JsonResponse({})
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def accept_invite(request: Request, user_id: int):
+    friends_service.accept_invite(request.user.id, user_id)
+    return JsonResponse({})
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def decline_invite(request: Request, user_id: int):
+    friends_service.decline_invite(request.user.id, user_id)
+    return JsonResponse({})
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def get_incoming_invites(request: Request):
+    return JsonResponse(
+        list(map(
+            lambda u: u.data,
+            friends_service.get_incoming_invites(request.user.id)
+        )),
+        safe=False
+    )
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def get_outcoming_invites(request: Request):
+    return JsonResponse(
+        list(map(
+            lambda u: u.data,
+            friends_service.get_outcoming_invites(request.user.id)
+        )),
+        safe=False
+    )
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def get_friends(request: Request):
+    return JsonResponse(
+        list(map(
+            lambda u: u.data,
+            friends_service.get_friends(request.user.id)
+        )),
+        safe=False
+    )
+
+
+class FriendView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request: Request, user_id: int):
+        return JsonResponse({
+            'status': friends_service.get_friend_status(request.user.id, user_id)
+        })
+
+    def delete(self, request: Request, user_id: int):
+        friends_service.remove_friend(request.user.id, user_id)
+        return JsonResponse({})
