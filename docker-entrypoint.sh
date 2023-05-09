3b57438dee569ba1c243164f7479a8ebb51e3954
arsuhinars <arsuhinars@gmail.com>
2023-05-09 22:21:08 +0400

    ✨ Added automated superuser creation

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,4 +4,13 @@ python manage.py collectstatic --noinput
 
 python manage.py migrate
 
+export SUPERUSER_NAME_FILE=/run/secrets/superuser_name
+export SUPERUSER_PASSWORD_FILE=/run/secrets/superuser_password
+
+if [ -f $SUPERUSER_NAME_FILE -a -f $SUPERUSER_PASSWORD_FILE ]; then
+export DJANGO_SUPERUSER_NAME=$(cat $SUPERUSER_NAME_FILE)
+export DJANGO_SUPERUSER_PASSWORD=$(cat $SUPERUSER_PASSWORD_FILE)
+python manage.py createsuperuser --noinput || echo 'Error occured while creating superuser'
+fi
+
 python manage.py runserver 0.0.0.0:$BACKEND_PORT $@
