3c11017ca31a1c5acdc925b761db103079897909
arsuhinars <arsuhinars@gmail.com>
2023-05-09 19:21:50 +0400

    📝 API schema update

--- api/urls.py
+++ api/urls.py
@@ -5,5 +5,5 @@ from api.views import get_user_by_id, register_user, search_users
 urlpatterns = [
     path('registration', register_user),
     path('user/<int:user_id>', get_user_by_id),
-    path('user/search', search_users)
+    path('users/search', search_users)
 ]
