c7f379df0a9c7df862c05ba51bd52158dd3346e2
arsuhinars <arsuhinars@gmail.com>
2023-05-09 18:53:00 +0400

    ✨ Added models module

--- api/models.py
+++ api/models.py
@@ -1,3 +1,53 @@
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import BaseUserManager
+from django.contrib.postgres.indexes import GinIndex, OpClass
 from django.db import models
+from django.db.models.functions import Upper
 
-# Create your models here.
+import api.config as config
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, name: str, password: str):
+        user = self.model(name=name)
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, name: str, password: str | None = ...):
+        user = self.create_user(
+            name=name,
+            password=password
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class UserEntity(AbstractBaseUser):
+    name = models.CharField(max_length=config.USER_NAME_LENGTH, unique=True)
+    outcoming_invites = models.ManyToManyField(
+        'self',
+        symmetrical=False,
+        related_name='incoming_invites'
+    )
+    friends = models.ManyToManyField('self', symmetrical=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'name'
+
+    @property
+    def is_staff(self):
+        return self.is_admin
+
+    class Meta:
+        indexes = [
+            GinIndex(
+                OpClass(Upper('name'), name='gin_trgm_ops'),
+                name='user_name_gin_idx'
+            ),
+            models.Index(fields=['name'])
+        ]
